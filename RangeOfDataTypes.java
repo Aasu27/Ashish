16a6c3b6130ca94d1f2a4d5b542b119b17f2e133
Aasu27 <83627697+Aasu27@users.noreply.github.com>
2021-05-04 18:31:40 +0530

    Add files via upload

---
+++ RangeOfDataTypes.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class RangeOfDataTypes{
+	public static void main(String[] args){
+		System.out.println("For int:");
+		System.out.println("Min:" + Integer.MIN_VALUE);
+		System.out.println("Max:" + Integer.MAX_VALUE);
+
+		System.out.println("For float:");
+		System.out.println("Min:" + Float.MIN_VALUE);
+		System.out.println("Max:" + Float.MAX_VALUE);
+
+		System.out.println("For short:");
+		System.out.println("Min:" + Short.MIN_VALUE);
+		System.out.println("Max:" + Short.MAX_VALUE);
+
+		System.out.println("For long:");
+		System.out.println("Min:" + Long.MIN_VALUE);
+		System.out.println("Max:" + Long.MAX_VALUE);
+
+		System.out.println("For double:");
+		System.out.println("Min:" + Double.MIN_VALUE);
+		System.out.println("Max:" + Double.MAX_VALUE);
+
+		System.out.println("For byte:");
+		System.out.println("Min:" + Byte.MIN_VALUE);
+		System.out.println("Max:" + Byte.MAX_VALUE);		
+	}
+}
